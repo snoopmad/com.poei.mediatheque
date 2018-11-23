0d6703045b757548562b254b0a7846803c144f24
Unknown <psyko623@gmail.com>
2018-11-23 09:52:49 +0100

    Arborescence projet

---
+++ mediathequeback/src/main/java/mediathequeback/Main.java
@@ -0,0 +1,10 @@
+package mediathequeback;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ mediathequefront/src/main/java/mediathequefront/Main.java
@@ -0,0 +1,9 @@
+package mediathequefront;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
